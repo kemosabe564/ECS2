58b615cd2e87f73a3ec633ae7fe856faf840edb3
kemosabe564 <liyiting.kemos@gmail.com>
2022-03-18 12:31:17 +0100

    ready to case 5

--- Code/case1.m
+++ Code/case1.m
@@ -18,7 +18,16 @@ LKAS.fh = 1/100;
 LKAS.n_ROI = 8;
 LKAS.contorller_type = 1;
 
-Case_index = 2;
+% case selection
+Case_index = 4;
+
+% plot setting 
+PATTERN = {1};
+
+Reference = 0;
+% simulation in seconds
+Simulation_time = 10; 
+Initial_value = 0.20;
 
 switch Case_index
     case 1
@@ -45,6 +54,7 @@ switch Case_index
         disp('Case 5:')
         LKAS.n_pipeline = 1;
         LKAS.n_parallelization = 1;
+        PATTERN={'s_1'};
         
     otherwise
         disp('invalid index, set as default case 1')
@@ -65,20 +75,17 @@ fprintf('System time analysis: tau=%.3f, h=%.3f\n',LKAS.tau, LKAS.h);
 LKAS_CS = augmentSystem(LKAS.tau, LKAS.h, LKAS.n_pipeline, LKAS_CS);
 
 LKAS_CS.R = 10;
-% temp = [0 1 10 1 0 0 0 0 0];
-temp = [1 1 1 0.01 0];
+
+temp = [0.76 0.22 0.61 0.015 0 0 0 0 ];
+% temp = [1 1 1 0.01 0];
+
+
 LKAS_CS.Q = 1*(temp') * temp;
 clear temp
 [LKAS_CS.K, LKAS_CS.F, LKAS_CS.K_T, LKAS_CS.T] = discreteTimeController(LKAS.tau, LKAS.h, LKAS_CS.Q, LKAS_CS.R, LKAS_CS);
 
 
 %% Simulation
-
-Reference = 0;
-PATTERN = {1};
-% simulation in seconds
-Simulation_time = 10; 
-Initial_value = 0.20;
 
 if LKAS.n_pipeline <= 1
     simulateDIC(Initial_value, LKAS.contorller_type, LKAS.h, LKAS.tau, LKAS_CS.Phi_aug, LKAS_CS.Gamma_aug, LKAS_CS.C_aug, LKAS_CS.K, LKAS_CS.F, PATTERN, Simulation_time, Reference, LKAS.fh);
